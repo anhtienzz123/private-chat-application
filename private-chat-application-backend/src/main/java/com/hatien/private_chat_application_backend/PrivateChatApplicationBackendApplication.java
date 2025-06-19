77a351b83470d829ee3cf373c6cdea0b40669dba
Tiên Huỳnh <anhtienzz123@gmail.com>
2025-06-20 03:04:46 +0700

    Add project private-chat-application-backend

---
+++ private-chat-application-backend/src/main/java/com/hatien/private_chat_application_backend/PrivateChatApplicationBackendApplication.java
@@ -0,0 +1,13 @@
+package com.hatien.private_chat_application_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrivateChatApplicationBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrivateChatApplicationBackendApplication.class, args);
+	}
+
+}
---
+++ private-chat-application-backend/src/test/java/com/hatien/private_chat_application_backend/PrivateChatApplicationBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hatien.private_chat_application_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrivateChatApplicationBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
